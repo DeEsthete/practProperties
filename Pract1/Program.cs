0458525b09c353d15751232fb12ffb99a6908bc1
JekaLion <djoni-01@mail.ru>
2017-12-04 17:41:38 +0600

    Добавьте файлы проекта.

---
+++ Pract1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите диапозон массива x, y");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            RangeOfArray array = new RangeOfArray(x, y);
+
+            Console.WriteLine("Заполните массив");
+            for (int i = x; i < y; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Массив");
+            for (int i = x; i < y; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pract1/RangeOfArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract1
+{
+    public class RangeOfArray
+    {
+        private int Start { get; set; }
+        private int Finish { get; set; }
+        private int[] _array;
+
+        public int this[int index]
+        {
+            get
+            {
+                if (Start < 0)
+                {
+                    return _array[(Start * -1) + index];
+                }
+                else if (Start > 0)
+                {
+                    return _array[index - Start];
+                }
+                return -1;
+            }
+
+            set
+            {
+                if (Start < 0)
+                {
+                    _array[(Start * -1) + index] = value;
+                }
+                else if (Start > 0)
+                {
+                    _array[index - Start] = value;
+                }
+            }
+        }
+
+        public RangeOfArray(int start, int finish)
+        {
+            Start = start;
+            Finish = finish;
+            if (start < 0 && finish < 0)
+            {
+                start = start * -1;
+                finish = finish * -1;
+                int x = start;
+                start = finish;
+                finish = x;
+            }
+            _array = new int[finish - start];
+        }
+    }
+}
